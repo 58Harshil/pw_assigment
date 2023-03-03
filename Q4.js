7d95dfa5ed586bdc212322426e7146d94251d639
Harshil Choudhary <112849807+58Harshil@users.noreply.github.com>
2023-03-03 22:27:24 +0530

    Create Q4.js

---
+++ Q4.js
@@ -0,0 +1,50 @@
+/* q1
+let traffic = "red";
+if(traffic=="red")
+{
+    console.log('You should stop');
+}
+else if(traffic=="Orange")
+{
+    console.log("you should wait till signal change")
+}
+else if(traffic=="green")
+{
+    console.log("you should run your viecial")
+}
+else
+{
+    confirm.log("invalid color")
+}
+*/
+/* q2
+let num1 = 20;
+let num2 = 15;
+if(num1>num2)
+{
+    console.log("num1 > num2")
+}
+else
+{
+    console.log("num1 < num2")
+}
+*/
+/* q3
+let num = 10
+if(num%5==0 && num%3==0){
+    console.log("FIzzBuzz")
+}
+else if (num%3==0)
+{
+    console.log("Fizz")
+}
+else if (num%5==0)
+{
+    console.log("Buzz")
+}
+else
+{
+    console.log("invalid input")
+}
+*/
+
