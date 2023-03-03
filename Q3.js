2bf2f524f9732fcacd677cb9c4e1237e280512ef
Harshil Choudhary <112849807+58Harshil@users.noreply.github.com>
2023-03-03 21:49:52 +0530

    Create Q3.js

---
+++ Q3.js
@@ -0,0 +1,48 @@
+/* q1
+number = 6
+for(let i = 1; i <= 10; i++) {
+    const result = i * number;
+    console.log(`${number} * ${i} = ${result}`);
+}
+*/
+/* question 2
+let num = 10;
+let num1 = 11;
+//+
+ console.log(num+num1 );
+ //-
+ console.log(num-num1);
+ //*
+ console.log(num*num1);
+ // /
+ console.log(num/num1);
+ //%
+ console.log(num%num1);
+ //** 
+ console.log(num**num1);
+ */
+/*q3
+ let length = 10;
+ let width = 20;
+let madercod = 2 * (length+width);
+console.log(madercod)
+*/
+
+let num1 = 12
+let num2 = 10
+//==
+console.log(num1==num2)
+//!=
+console.log(num1!=num2)
+//===
+console.log(num1===num2)
+//!==
+console.log(num1!==num2)
+//>
+console.log(num1>num2)
+//>=
+console.log(num1>=num2)
+//<
+console.log(num1<num2)
+//<=
+console.log(num1<=num2)
