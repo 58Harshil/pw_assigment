fe513c41b3055bd9dd60c0a335150467a4bb9152
Harshil Choudhary <112849807+58Harshil@users.noreply.github.com>
2023-03-03 23:22:27 +0530

    Create Q9.js

---
+++ Q9.js
@@ -0,0 +1,14 @@
+/* q1
+let number = 5
+for(let i = 1; i <= 10; i++) {
+    const result = i * number;
+    console.log(`${number} * ${i} = ${result}`);
+}
+*/
+
+let number = 10;
+for(let i=1; i<=number; i++){
+    if(i%2==0){
+        console.log(i)
+    }
+}
