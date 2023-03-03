0a56938011e3ff47181afe053d07cf234abe7a69
Harshil Choudhary <112849807+58Harshil@users.noreply.github.com>
2023-03-03 23:06:43 +0530

    Create Q6.js

---
+++ Q6.js
@@ -0,0 +1,76 @@
+/* q1
+const day = "tuesday"
+let weekend
+switch (day) {
+    case "monday":
+        console.log("there are 5 days until weekend")
+        break;
+     case "tuesday":
+            console.log("there are 4 days until weekend")
+            break;
+    case "wed":
+                console.log("there are 3 days until weekend")
+                break;
+
+                case "thusday":
+                    console.log("there are 2 days until weekend")
+                    break;
+                    case "friday":
+                        console.log("there are 1 days until weekend")
+                        break;
+                        case "saterday":
+                           case "sunday":
+                            console.log("there are 0 days until weekend")
+                            break;
+
+    default:
+        weekend = "invalid day"
+        break;
+}
+*/
+
+/* q2
+const month = 5;
+switch (month){
+    case 1 :
+        console.log("Jan")
+        break;
+        case 2 :
+        console.log("feb")
+        break;
+        case 3 :
+        console.log("march")
+        break;
+        case 4 :
+        console.log("april")
+        break;
+        case 5 :
+        console.log("may")
+        break;
+        case 6 :
+        console.log("june")
+        break;
+        case 7 :
+        console.log("july")
+        break;
+        case 8 :
+        console.log("august")
+        break;
+        case 9 :
+        console.log("september")
+        break;
+        case 10 :
+        console.log("october")
+        break;
+        case 11 :
+        console.log("noverber")
+        break;
+        case 12 :
+        console.log("december")
+        break;
+        default:
+            console.log("invaid month")
+}
+*/
+
+
