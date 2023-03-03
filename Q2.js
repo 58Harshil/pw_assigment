285f95837ba151110b4e6469c3f876711d4515d3
Harshil Choudhary <112849807+58Harshil@users.noreply.github.com>
2023-03-03 18:57:49 +0530

    Create Q2.js

---
+++ Q2.js
@@ -0,0 +1,40 @@
+//1.
+let var1 = "i am good"
+console.log(typeof var1);
+//2.
+//i)
+let var2 = 9;
+console.log(typeof var2);
+//ii)
+let var3 = 9.3;
+console.log(typeof var3);
+//iii)
+let var4 = Number.POSITIVE_INFINITY;
+console.log(typeof var4);
+//iv)
+let var5 = NaN;
+console.log(typeof var5);
+
+
+//q3
+let var6 = 9999n;
+console.log(typeof var6);
+//q4
+let var7 = true;
+console.log(typeof var7);
+//q5
+let var8 = undefined;
+console.log(typeof var8);
+//q6
+let var9 = Symbol("hello");
+console.log(typeof var9);
+//q7
+let var10 = null;
+console.log(typeof var10);
+//q8
+//i)
+let var11 = [1,2,"pw"];
+console.log(typeof var11);
+//ii)
+let var12 = {name:"harshil", course:"FSWD"};
+console.log(typeof var12);
