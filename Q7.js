858d1dbaf337d6494c5e49f42f269555e97a7ad3
Harshil Choudhary <112849807+58Harshil@users.noreply.github.com>
2023-03-03 23:13:55 +0530

    Create Q7.js

---
+++ Q7.js
@@ -0,0 +1,16 @@
+/* q1
+let number = 0
+number == 0
+? console.log("zero")
+:number>0
+? console.log("positive")
+:console.log("negative")
+*/
+
+let num = 10
+let num1 = 10
+num1 == num
+?console.log("equal")
+:num>num1
+?console.log(num)
+:console.log(num1)
